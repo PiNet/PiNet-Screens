e24723b22be70d56a15cf9998f750ea7073e7e9b
gbaman <gbaman@users.noreply.github.com>
2019-02-18 12:18:43 +0000

    Fix formatting

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -10,6 +11,7 @@ def home():
 @app.route("/test/<data>")
 def para_test(data):
     return "The data sent was {}".format(data)
+
 
 if __name__ == '__main__':
     app.run()

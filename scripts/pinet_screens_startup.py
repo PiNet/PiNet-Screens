2324cc958842b89d8f618a99e1d72afaf1cc0b40
gbaman <gbaman@users.noreply.github.com>
2019-02-20 13:47:58 +0000

    Fix issue with screens startup script around mac addresses

--- scripts/pinet_screens_startup.py
+++ scripts/pinet_screens_startup.py
@@ -10,8 +10,8 @@ server_address = "server" # Default LTSP IP address maps to server
 def get_mac_address():
     interfaces = netifaces.interfaces()
     for interface in interfaces:
-        if "eth0" in  interface or "en" in interface:
-            mac_address = netifaces.ifaddresses(interface)[netifaces.AF_LINK]["addr"]
+        if interface.startswith("eth0") or interface.startswith("en"):
+            mac_address = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]["addr"]
             return mac_address
 
 
@@ -19,7 +19,7 @@ def report_back(mac_address):
     r = requests.post("http://{}/endpoint/update".format(server_address), json={"mac_address":mac_address, "hostname":socket.getfqdn()})
 
 
-for interface in os.walk("/sys/class/net/"):
+for interface in netifaces.interfaces():
     mac_address = get_mac_address()
     for file in os.listdir("scripts/"):
         if file == mac_address:

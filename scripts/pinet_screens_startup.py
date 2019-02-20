b05403a354c0f851402b6b0ddeec4efbc24e9b21
gbaman <gbaman@users.noreply.github.com>
2019-02-20 16:22:02 +0000

    Add background thread to Pi script

--- scripts/pinet_screens_startup.py
+++ scripts/pinet_screens_startup.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
 import os
+import time
+
 import requests
 import socket
 import netifaces
+import hashlib
+from threading import Thread
+
+
 server_address = "server" # Default LTSP IP address maps to server
+script_root = "/usr/local/bin/scripts/"
+file_hash = ""
+ran_script_path = ""
+
+
+def background_thread():
+    time.sleep(30)
+    while True:
+        report_back(mac_address=get_mac_address())
+        h = hash_file(ran_script_path)
+        if h != file_hash:
+            os.system("sudo reboot")
+        time.sleep(60)
 
 
 def get_mac_address():
@@ -15,12 +34,32 @@ def get_mac_address():
             return mac_address
 
 
+def hash_file(path):
+    global file_hash
+    hasher = hashlib.md5()
+    with open(path, 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
+
+
 def report_back(mac_address):
     r = requests.post("http://{}/endpoint/update".format(server_address), json={"mac_address":mac_address, "hostname":socket.getfqdn()})
 
 
-for interface in netifaces.interfaces():
+def main():
     mac_address = get_mac_address()
-    for file in os.listdir("scripts/"):
+    report_back(mac_address)
+    thread = Thread(target=background_thread())
+    thread.daemon = True
+    thread.start()
+    for file in os.listdir(script_root):
         if file == mac_address:
             print("Found match! {}".format(file))
+            os.system("bash {}{}".format(script_root, file))
+
+    thread.join()
+
+
+if __name__ == '__main__':
+    main()
